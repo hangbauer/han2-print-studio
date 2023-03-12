119167b4f3b87f30d64a96f64d11b1de96d717aa
Hang <hangbauer@live.com>
2023-03-12 21:23:03 +0700

    update

---
+++ Pages/Master/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace han2_print_studio.Pages.Master
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
